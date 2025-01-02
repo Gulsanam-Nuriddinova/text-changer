ddf4861c0f43b542a81efcae4c35688f30efe981
Gulsanam-Nuriddinova <gulsanamnuriddinova56@gmail.com>
2025-01-02 01:53:26 -0800

    init

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const elAsosiy = document.getElementById('asosiy')
+const elSoz = document.getElementById('soz')
+const elOrniga = document.getElementById('orniga')
+const elBtn = document.getElementById('btn')
+const elNatija = document.getElementById('natija')
+
+elBtn.addEventListener('click' , () => {
+    const elValue1 = elAsosiy.value
+    const elValue2 = elSoz.value
+    const elValue3 = elOrniga.value
+
+    elNatija.textContent = elValue1.replaceAll(elValue2, elValue3)
+})
